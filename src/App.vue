424d0932229cb2a9b282c82ffb2c592dd403da5f
et4891 <et1103@hotmail.com>
2020-09-19 05:09:56 -0700

    added loader

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     <TodoInput
       @onAddEmit="onAdd"
     />
+
+    <!-- add loader -->
+    <div v-if="!todos">
+      <p class="loader"></p>
+    </div>
     <TodoList
+      v-else
       :todos="todos"
       @onRemoveEmit="onRemove"
     />
@@ -20,7 +26,7 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [],
+      todos: null,  // using null to start with which will get the loader going
     }
   },
   components: {
@@ -59,5 +65,18 @@ export default {
 </script>
 
 <style scoped>
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  margin: auto;
+}
 
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
